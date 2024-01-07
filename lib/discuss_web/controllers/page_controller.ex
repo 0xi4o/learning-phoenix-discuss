07870f8026d2f5a77e377be00718e02a4c76192d
Ilango <rajagopalilango@gmail.com>
2024-01-07 13:29:33 +0530

    📦 NEW: Initial commit

---
+++ lib/discuss_web/controllers/page_controller.ex
@@ -0,0 +1,12 @@
+defmodule DiscussWeb.PageController do
+  use DiscussWeb, :controller
+
+  def home(conn, _params) do
+    # The home page is often custom made,
+    # so skip the default app layout.
+    render(conn, :home, layout: false)
+    # conn
+    # |> put_layout(html: :admin)
+    # |> render(:home)
+  end
+end
